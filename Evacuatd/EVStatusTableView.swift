03e25c5f55a021725ced64317a2ffd7a33628036
Patrick <patrickbellot77@gmail.com>
2016-08-29 11:23:38 -0500

    Setting up initial view and status table view

---
+++ Evacuatd/EVStatusTableView.swift
@@ -0,0 +1,45 @@
+//
+//  EVStatusTableView.swift
+//  Evacuatd
+//
+//  Created by Patrick Bellot on 8/29/16.
+//  Copyright © 2016 Bell OS, LLC. All rights reserved.
+//
+
+import UIKit
+
+class EVStatusTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StatusCell", for: indexPath) 
+        return cell
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
